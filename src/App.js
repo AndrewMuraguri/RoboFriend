68e49e35d314221936c287e8bb59ba2786b456cb
AndrewMuraguri <awakabs@gmail.com>
2023-02-22 15:52:19 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox'
 import Scroll from './Scroll'
+import ErrorBoundary from './ErrorBoundary'
 
 
 
@@ -40,7 +41,9 @@ class App extends Component {
                 <h1>RoboFriends</h1>
                 <SearchBox searchChange={this.onSearchChange} />
                 <Scroll>
+                    <ErrorBoundary>
                     <CardList robots={filteredRobots} />
+                    </ErrorBoundary>
                 </Scroll>
             </div>
 
